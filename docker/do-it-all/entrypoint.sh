4584ac5aef6bd3704b220e4e6473287ad453340e
Carl Furrow <hello@carlfurrow.com>
2022-01-23 07:55:38 -0500

    docker do-it-all doe snot work

--- docker/do-it-all/entrypoint.sh
+++ docker/do-it-all/entrypoint.sh
@@ -4,5 +4,8 @@ TIMESTAMP_FILE="$1"
 INPUT_FILE="$2"
 CLIPS_DIR="$3"
 
+echo "This does not work yet."
+exit 1
+
 /usr/local/bin/chapterize "$TIMESTAMP_FILE" "$INPUT_FILE"
 /usr/local/bin/create-clips --from_file "$TIMESTAMP_FILE" "$INPUT_FILE" "$CLIPS_DIR"
