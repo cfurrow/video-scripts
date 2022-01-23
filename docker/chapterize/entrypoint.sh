c8f85b9940e71a35eb2bc25b988df459b16c94c9
Carl Furrow <hello@carlfurrow.com>
2022-01-22 22:56:23 -0500

    quote some more

--- docker/chapterize/entrypoint.sh
+++ docker/chapterize/entrypoint.sh
@@ -3,6 +3,6 @@
 TRANSCRIPT_FILE="$1"
 INPUT_FILE="$2"
 
-/usr/local/bin/generate-mp4-chapter-metadata $TRANSCRIPT_FILE
-/usr/local/bin/set-mp4-chapter-metadata $INPUT_FILE
+/usr/local/bin/generate-mp4-chapter-metadata "$TRANSCRIPT_FILE"
+/usr/local/bin/set-mp4-chapter-metadata "$INPUT_FILE"
 
