e41de50799c222c1f3842c963cbc51e5faf6ba49
Carl Furrow <hello@carlfurrow.com>
2022-01-22 22:55:44 -0500

    set inputs to vars

--- docker/chapterize/entrypoint.sh
+++ docker/chapterize/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-/usr/local/bin/generate-mp4-chapter-metadata "$1"
-/usr/local/bin/set-mp4-chapter-metadata "$2"
+TRANSCRIPT_FILE="$1"
+INPUT_FILE="$2"
+
+/usr/local/bin/generate-mp4-chapter-metadata $TRANSCRIPT_FILE
+/usr/local/bin/set-mp4-chapter-metadata $INPUT_FILE
 
