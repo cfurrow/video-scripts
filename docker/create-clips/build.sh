9e56dc3ab02d8fed0954c7488db52d69d828b3e8
Carl Furrow <hello@carlfurrow.com>
2022-01-23 10:53:17 -0500

    make docker builds work with gem

--- docker/create-clips/build.sh
+++ docker/create-clips/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cp ../../create-clips .
+
+mkdir ./build
+
+# get latest version of video_scripts gem
+latest_gem=`ls -t ../../pkg/video_scripts* | head -1`
+cp "$latest_gem" ./build/video_scripts.gem
+
 docker build -t create-clips .
